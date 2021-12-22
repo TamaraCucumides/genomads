f289ec15602c18352314abb804b412e16fb4c4f0
Tamara Cucumides <tacucumides@uc.cl>
2021-12-22 15:27:45 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='genomaviz',
     packages=find_packages(include=['genomaviz']),
-    version='0.1.0',
+    version='0.1.1',
     description='Python library for Genomawork visualizations',
     author='Tamara Cucumides - Pablo Larrea',
     license='MIT',
