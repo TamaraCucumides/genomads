33ec23101778b0904152134e4ee668c9b004276a
Pablo Larrea <54015114+PalboPls@users.noreply.github.com>
2022-02-14 11:05:00 -0300

    Update ver 0.1.2
    Eho
    Palettes updated
    Added new_plots (such as gender_plot)
    Added support for custom_genomes (aka class Polinomio)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='genomaviz',
     packages=find_packages(include=['genomaviz']),
-    version='0.1.1',
+    version='0.1.2',
     description='Python library for Genomawork visualizations',
     author='Tamara Cucumides - Pablo Larrea',
     license='MIT',
